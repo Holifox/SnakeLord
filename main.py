ac5cf0aea157f091ccb72907ff5cc3e4ea12e7a1
Holifox <Email>
2022-07-06 21:16:53 +0300

    game field extension

--- main.py
+++ main.py
@@ -9,47 +9,59 @@ BORDER_COLOR = (10, 70, 70)
 FONT_COLOR = (10, 80, 80)
 
 pg.init()
-# ctypes.windll.user32.SetProcessDPIAware()
+ctypes.windll.user32.SetProcessDPIAware()
 face = pg.display.set_mode((0, 0), pg.FULLSCREEN)
 W, H = face.get_size()
 pg.display.set_caption("Snake Lord")
 font = pg.font.SysFont('Verdana', 40)
 clock = pg.time.Clock()
 
-face2 = pg.Surface((W*3, H*3), pg.SRCALPHA)
-face2_x, face2_y = -W, -H
 
 # Load the background image
 back = pg.image.load(r'back.png')
+face2 = pg.Surface((W*3, H*3))
+face2.blit(back, (0, 0))
+face2_pos = (-W, -H)
 run = True
-FPS = 15
+FPS = 30
 
 
 class SnakeItem(pg.sprite.Sprite):
     def __init__(self, x, y, r) -> None:
         super().__init__()
         surf = pg.Surface((2*r, 2*r), pg.SRCALPHA)
-        pg.draw.circle(surf, (0, 30, 30), (r, r), r)
+        pg.draw.circle(surf, (0, 100, 80), (r, r), r)
         self.image = surf
-        self.rect = (x, y, x + r, y + r)
+        self.rect = (x, y, r*2, r*2)
         self.r = r
 
-
+    def clear(self) -> None:
+        global face2, back
+        x, y, w, h = tuple(map(int, self.rect))
+        face2.blit(back, (x, y), self.rect)
+        
+        
 class Snake():
     def __init__(self, length: int, dencity: int, thickness: int) -> None:
-        # pg.sprite.Group.__init__(self, *snake_items)
+        # pg.sprite.Group.__init__(self)
         num_items = length // dencity
         items = []
         for i in range(num_items):
-            item = SnakeItem(W/2-i*dencity, H/2, thickness)
+            item = SnakeItem(W*1.5 - i*dencity, H*1.5, thickness)
             items.append(item)
         self.items = items
         self.dencity = dencity
         self.thickness = thickness
+        track_item = items[0]
+        self.track_item_pos = track_item.rect[:2]
+
 
     def move(self) -> None:
+        global face2, face2_pos
         head_item = self.items[0]
         mouse_x, mouse_y = pg.mouse.get_pos()
+        mouse_x -= face2_pos[0]
+        mouse_y -= face2_pos[1]
         head_item_x, head_item_y = head_item.rect[:2]
 
         dist_x, dist_y = mouse_x - head_item_x, mouse_y - head_item_y
@@ -61,35 +73,52 @@ class Snake():
         new_item_y = head_item_y + self.dencity * sin
         new_item = SnakeItem(new_item_x, new_item_y, head_item.r)
         self.items.insert(0, new_item)
-        self.items.pop()
+        tail_item = self.items.pop()
+        tail_item.clear()
 
-    def draw(self, surface):
+        new_track_item = self.items[0]
+        x2, y2 = new_track_item.rect[:2]
+        x1, y1 = self.track_item_pos
+        self.track_item_shift = (x2 - x1, y2 - y1)
+        self.track_item_pos = (x2, y2)
+
+    def draw(self, surface: pg.Surface):
         for item in self.items:
             x, y = item.rect[:2]
             surface.blit(item.image, (x, y))
 
-        
+
+
 main_snake = Snake(
     length = 300, 
     dencity = 7, 
     thickness = 15
     )
-    
+
+def drag_screen():
+    global face2, face, main_snake, face2_pos
+
+    dx, dy = main_snake.track_item_shift
+    # face.scroll(int(-dx), int(-dy))
+    x, y = face2_pos
+    face2_pos = (x - dx, y - dy)
 
 def game():
     global main_menu, run, face, main_snake
 
     main_menu.disable()
+    face.blit(face2, face2_pos)
     while run:
         events = pg.event.get()
         for event in events:
             if event.type == pg.QUIT:
                 exit()
 
-        face.blit(back, (0, 0))
-        face.blit(face2, (face2_x, face2_y))
         main_snake.move()
-        main_snake.draw(face)   
+        main_snake.draw(face2)   
+        drag_screen()
+        face.fill((0, 0, 0))
+        face.blit(face2, face2_pos)
         pg.display.update()
         clock.tick(FPS)
 
@@ -116,7 +145,6 @@ main_menu = pg_menu.menu.Menu(
 
 # Add buttons on main_menu
 main_menu.add.button('Start game', game)
-main_menu.add.button('Records')
 main_menu.add.button('Exit', pg_menu.events.EXIT)
 
 def set_size(widget) -> None:
@@ -136,6 +164,7 @@ for menu in [main_menu]:
     decorator.add_line((-w - 2, h), (-w - 2, -h), color = BACK_COLOR, width = 4)
 
     for widget in menu._widgets:
+
         set_size(widget)
 
-main_menu.mainloop(face, bgfun = lambda: face.blit(back, (0, 0)))
+main_menu.mainloop(face, bgfun = lambda: face.blit(face2, (0, 0)))
