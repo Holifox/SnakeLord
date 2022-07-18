87e015aa70ca29375d0b15704ab06e3fe2c013f1
Holifox <Email>
2022-07-18 10:17:00 +0300

    Add restart menu and bounds control

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ clock = pg.time.Clock()
 
 # Load the background image
 back = pg.image.load(r'back.png')
-surf2 = pg.Surface((W*3, H*3)) 
+surf2 = pg.Surface(back.get_size()) 
 surf2.blit(back, (0, 0))
 surf2_pos = (W//2, H//2)
 run = True
@@ -43,22 +43,23 @@ class SnakeItem(pg.sprite.Sprite):
         self.rect = pg.Rect(x, y, r*2, r*2)
 
         
-class Snake():
+class Snake:
     def __init__(self, length: int) -> None:
-        # pg.sprite.Group.__init__(self)
         thickness = 2*sqrt(length)
         dencity = thickness // 2
         items = []
+        Cx, Cy = surf2.get_rect().center
         for i in range(length):
-            item = SnakeItem(W*1.5 - i*dencity, H*1.5, thickness)
+            item = SnakeItem(Cx - i*dencity, Cy, thickness)
             items.append(item)
         self.items = items
         self.len = length
         self.dencity = dencity
         self.thickness = thickness
+        self.mouse_pos = (0, 0)
 
     def add_new_item(self):
-        global surf2, surf2_pos
+        global surf2, surf2_pos, run
 
         # Take coords of snake's first item
         head_item = self.items[0]
@@ -66,24 +67,31 @@ class Snake():
 
         # determine mouse cursor position 
         mouse_x, mouse_y = pg.mouse.get_pos()
-        surf2_w, surf2_h = surf2.get_size()
-        mouse_x += -surf2_pos[0] + 0.5 * surf2_w
-        mouse_y += -surf2_pos[1] + 0.5 * surf2_h
+        if mouse_x != self.mouse_pos[0] and mouse_y != self.mouse_pos[1]:
+            surf2_w, surf2_h = surf2.get_size()
+            mouse_x += -surf2_pos[0] + 0.5 * surf2_w
+            mouse_y += -surf2_pos[1] + 0.5 * surf2_h
 
-        # Count coords for snake's new item
-        dist_x, dist_y = mouse_x - head_item_x, mouse_y - head_item_y
-        dist = hypot(dist_x, dist_y)
-        cos = dist_x / dist
-        sin = dist_y / dist
-        dx, dy = round(self.dencity * cos), round(self.dencity * sin)
+            # Count coords for snake's new item
+            dist_x, dist_y = mouse_x - head_item_x, mouse_y - head_item_y
+            dist = hypot(dist_x, dist_y)
+            cos = dist_x / dist
+            sin = dist_y / dist
+            dx, dy = round(self.dencity * cos), round(self.dencity * sin)
+        else:   # if mouse cursor haven't moved
+            dx, dy = self.shift
+
         new_item_x = head_item_x + dx
         new_item_y = head_item_y + dy
 
         # Set a new item and discard the last
         new_item = SnakeItem(new_item_x, new_item_y, self.thickness)
+        if not surf2.get_rect().contains(new_item.rect):
+            run = False
         self.items.insert(0, new_item)
     
-        return (dx, dy)
+        self.shift = (dx, dy)
+        self.mouse_pos = (mouse_x, mouse_y)
 
     def check_bonus(self, bonuses):
         head_item = self.items[0]
@@ -94,7 +102,6 @@ class Snake():
         else:
             self.len = len(self.items)
             self.thickness = 2*sqrt(self.len)
-            # self.dencity = self.thickness // 2
 
             bonus = test_collide
             bonus.kill()
@@ -139,7 +146,7 @@ def set_bonuses(num: int) -> pg.sprite.Group:
     return bonuses
 
 
-def blit_surf2(dx, dy):
+def show_room(dx, dy):
     global surf, surf2, surf2_pos, main_snake
     x, y = surf2_pos
     surf2_pos = (x - dx, y - dy)
@@ -161,32 +168,45 @@ def blit_surf2(dx, dy):
 
 
 def game():
-    global main_menu, run, main_snake, surf2_pos
-
+    global main_menu, run, main_snake, surf2_pos, surf2
+    
     main_menu.disable()
+    end_menu.disable()
+    surf2.blit(back, (0, 0))
+    surf2_pos = (W//2, H//2)
     bonuses = set_bonuses(1000)
     bonuses.draw(surf2)
+    pg.display.update()
+    
     main_snake = Snake(50)
 
+    run = True
     while run:
         events = pg.event.get()
         for event in events:
             if event.type == pg.QUIT:
                 exit()
 
-        dx, dy = main_snake.add_new_item()
+        main_snake.add_new_item()
         main_snake.check_bonus(bonuses)
         main_snake.draw()
 
         surf.fill((0, 0, 0))
-        blit_surf2(dx, dy)
+        show_room(*main_snake.shift)
         text_surf = font.render(f'Score: {main_snake.len}', 1, (10, 100, 100))
         surf.blit(text_surf, (50, 30))
         
         pg.display.update()
         clock.tick(FPS)
 
-    
+    end_menu.enable()
+    # surf2_rect = surf2.get_rect(center = surf2_pos)
+    # end_menu.mainloop(surf, bgfun = lambda: surf.blit(surf2, surf2_rect))
+    while end_menu.is_enabled():
+        events = pg.event.get()
+        end_menu.update(events)
+        end_menu.draw(surf)
+        pg.display.update()
 
 # ------------------------------ Menus ------------------------------ #
 
@@ -207,9 +227,18 @@ main_menu = pg_menu.menu.Menu(
     mouse_motion_selection = True,
     theme = theme1)
 
+end_menu = pg_menu.menu.Menu(
+    'Game over', 600, 400, 
+    mouse_motion_selection = True,
+    theme = theme1, enabled = False)
+
 # Add buttons on main_menu
 main_menu.add.button('Start game', game)
 main_menu.add.button('Exit', pg_menu.events.EXIT)
+
+# Add buttons on end_menu
+end_menu.add.button('Try again', game)
+end_menu.add.button('Exit', pg_menu.events.EXIT)
 
 def set_size(widget) -> None:
     ''' Costomise widgets '''
@@ -219,7 +248,7 @@ def set_size(widget) -> None:
     widget.set_padding((dh, dw))
 
 # Castomize menus
-for menu in [main_menu]:
+for menu in [main_menu, end_menu]:
     w, h = menu.get_size()
     w, h = w//2, h//2
     decorator = menu.get_decorator()
@@ -228,7 +257,7 @@ for menu in [main_menu]:
     decorator.add_line((-w - 2, h), (-w - 2, -h), color = BACK_COLOR, width = 4)
 
     for widget in menu._widgets:
-
         set_size(widget)
 
-main_menu.mainloop(surf, bgfun = lambda: surf.blit(surf2, (0, 0)))
+surf2_rect = surf2.get_rect(center = surf2_pos)
+main_menu.mainloop(surf, bgfun = lambda: surf.blit(surf2, surf2_rect))
